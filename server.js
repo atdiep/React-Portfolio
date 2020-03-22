cb8b2c5b0b41365777ff78a7911aa89e06bcf9e3
Arnold Diep <tungd@uw.edu>
2020-03-21 20:46:27 -0700

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.listen(PORT, function() {
+  console.log(`🌎  ==> Now listening on PORT ${PORT}!`);
 });
