c0874ff08f9bb9ef567ec74b13d645a1d3dae785
Arnold Diep <tungd@uw.edu>
2020-03-24 20:29:41 -0700

    trail 13

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join("portfolio", "build")));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./portfolio/public/index.html"));
-})
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./portfolio/public/index.html"));
+// })
 app.listen(PORT, function() {
   console.log(`🌎  ==> Now listening on PORT ${PORT}!`);
 });
